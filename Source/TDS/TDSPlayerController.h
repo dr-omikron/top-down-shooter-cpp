bb36b4983095942f92958cece3594b70e7519857
dr-omikron <andrey163.v@yandex.ru>
2023-10-01 17:16:17 +0400

    Changed Player Controls
    - Changed TDSPlayerController cpp files. Changed template control to control WASD

--- Source/TDS/TDSPlayerController.h
+++ Source/TDS/TDSPlayerController.h
@@ -3,13 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Templates/SubclassOf.h"
-#include "GameFramework/PlayerController.h"
 #include "InputActionValue.h"
+#include "GameFramework/PlayerController.h"
 #include "TDSPlayerController.generated.h"
-
-/** Forward declaration to improve compiling times */
-class UNiagaraSystem;
 
 UCLASS()
 class ATDSPlayerController : public APlayerController
@@ -18,48 +14,22 @@ class ATDSPlayerController : public APlayerController
 
 public:
 	ATDSPlayerController();
-
-	/** Time Threshold to know if it was a short press */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
-	float ShortPressThreshold;
-
-	/** FX Class that we will spawn when clicking */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
-	UNiagaraSystem* FXCursor;
-
-	/** MappingContext */
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
 	class UInputMappingContext* DefaultMappingContext;
 	
-	/** Jump Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
-	class UInputAction* SetDestinationClickAction;
+	class UInputAction* Move;
 
-	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
-	class UInputAction* SetDestinationTouchAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character")
+	class ATDSCharacter* PlayerCharacter;
 
 protected:
-	/** True if the controlled character should navigate to the mouse cursor. */
-	uint32 bMoveToMouseCursor : 1;
-
-	virtual void SetupInputComponent() override;
 	
-	// To add mapping context
+	virtual void SetupInputComponent() override;
 	virtual void BeginPlay();
 
-	/** Input handlers for SetDestination action. */
-	void OnInputStarted();
-	void OnSetDestinationTriggered();
-	void OnSetDestinationReleased();
-	void OnTouchTriggered();
-	void OnTouchReleased();
-
-private:
-	FVector CachedDestination;
-
-	bool bIsTouch; // Is it a touch device
-	float FollowTime; // For how long it has been pressed
+	void OnMove(const FInputActionValue& Value);
 };
 
 
